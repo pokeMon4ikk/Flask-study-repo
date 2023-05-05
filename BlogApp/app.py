72ce02dae81f6a1b4c6366982cd2b734e24c90b2
pavel <pasha050598@mail.ru>
2023-05-05 16:50:50 +0300

    Lesson-2

--- BlogApp/app.py
+++ BlogApp/app.py
@@ -1,10 +1,15 @@
-from time import time
-from flask import Flask, request, g
-import socket
+from flask import Flask, render_template
+from BlogApp.views.users import users_app
+from BlogApp.views.articles import articles_app
+
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
+
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return 'Hello'
+    return render_template("index.html")
+
