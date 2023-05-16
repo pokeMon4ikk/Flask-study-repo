898686afe57b2694baaa6a045c2ba0f9fc65c2b2
pavel <pasha050598@mail.ru>
2023-05-16 15:37:12 +0300

    Lesson-4

--- blog/views/users.py
+++ blog/views/users.py
@@ -1,24 +1,26 @@
 from flask import Blueprint, render_template
+
 from werkzeug.exceptions import NotFound
 
 users_app = Blueprint("users_app", __name__)
-
 USERS = {
     1: "James",
     2: "Brian",
     3: "Peter",
 }
-
-
 @users_app.route("/", endpoint="list")
 def users_list():
-    return render_template("../templates/users/list.html", users=USERS)
+    from blog.models import User
+    users = User.query.all()
+    return render_template("users/list.html", users=users)
+
 
 
 @users_app.route("/<int:user_id>/", endpoint="details")
 def user_details(user_id: int):
-    try:
-        user_name = USERS[user_id]
-    except KeyError:
+    from blog.models import User
+    user = User.query.filter_by(id=user_id).one_or_none()
+    if user is None:
         raise NotFound(f"User #{user_id} doesn't exist!")
-    return render_template('users/details.html', user_id=user_id, user_name=user_name)
+
+    return render_template('users/details.html', user=user)
